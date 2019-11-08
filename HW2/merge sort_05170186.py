616626b721b7a412ba5f8b2013c973ca78d88f8a
cream1117 <55724139+cream1117@users.noreply.github.com>
2019-11-08 23:24:26 +0800

    Update merge sort_05170186.py

--- HW2/merge sort_05170186.py
+++ HW2/merge sort_05170186.py
@@ -1,6 +1,7 @@
-num = [1,4,6,2,11,15,23]  #設定數字
-
-def mergesort(listneedtosort):  #定義mergesort這個函數
+#import random    
+num = [1,4,6,2,11,15,23] #設定num
+class Solution(object):
+    def mergesort(listneedtosort):  #定義mergesort這個函數
 	index1 = index2 = index3 = 0  
 
 	if len(listneedtosort) > 1:   #假設list超過一個依上的字元
@@ -29,5 +30,9 @@ def mergesort(listneedtosort):  #定義mergesort這個函數
 			index2 = index2 + 1
 			index3 = index3 + 1
 
+                return listneedtosort
+print(num)
+output = Solution().mergesort(num)
+print(output)
 
 
